12618ee323ceee91246ed9e4f26bfd41e00ef943
Daniel Bittman <danielbittman1@gmail.com>
2016-07-23 15:56:27 -0700

    Call global constructors from assembly start code.

--- core/main.c
+++ core/main.c
@@ -1,10 +1,21 @@
 #include <debug.h>
 #include <memory.h>
-void _init(void);
-void kernel_main(void)
+
+/* functions called from here expect virtual memory to be set up. However, functions
+ * called from here cannot rely on global contructors having run, as those are allowed
+ * to use memory management routines, so they are run after this.
+ */
+void kernel_early_init(void)
 {
 	mm_init();
-	_init();
+}
+
+/* at this point, memory management, interrupt routines, global constructors, and shared
+ * kernel state between nodes have been initialized. Now initialize all application processors
+ * and per-node threading.
+ */
+void kernel_main(void)
+{
 	printk("Kernel main %llx!\n", __round_up_pow2(0x1001));
 
 	panic("init completed");
