b0ae16b605ac57a3f75f42770ff140f76fd5b270
Daniel Bittman <danielbittman1@gmail.com>
2016-08-05 11:51:54 -0700

    Document trace system.

--- include/trace.h
+++ include/trace.h
@@ -9,6 +9,7 @@ struct trace {
 };
 
 #define TRACE_INITIALIZER(n,e) (struct trace) { .enabled = e, .name = n }
+
 #if CONFIG_DEBUG
 
 static inline bool __trace(struct trace *trace, const char *msg, ...)
@@ -33,13 +34,30 @@ struct trace_node {
 
 static inline void __trace_cleanup(struct trace_node *node)
 {
+	/* if we did the trace and we're exiting a scope, print a message */
 	if(!node->cleanup || !node->did)
 		return;
 	printk("%*s[%s] %s: function returned\n", --trace_indent_level, "", node->trace->name, node->fn);
 }
 
+/* usage: TRACE(trace-name, tracing-scope, printf-like-message, ...)
+ * trace-name is a pointer to a struct trace, so that sets of traces may be disabed or enabled at runtime.
+ * tracing-scope, if false, will simply print a trace message at the current indent level.
+ *                if true,  will print a message when the scope ends as well, and will increment indent level.
+ * This lets you do stuff like:
+ * void foo() { TRACE(&t, true, "test! %d", 4); ... ; TRACE(&t, false, "something happened"); }
+ * which will print out:
+ * [trace] foo: test! 4
+ *  [trace] foo: something happened
+ * [trace] foo: function returned
+ */
 #define TRACE(t,f,m,...) \
-	__cleanup(__trace_cleanup) struct trace_node __concat(__trace_node, __COUNTER__) = {.did = __trace(t, "%*s[%s] %s: " m "\n", f ? trace_indent_level++ : trace_indent_level, "", (t)->name, __FUNCTION__, ##__VA_ARGS__), .cleanup = f, .trace = (t), .fn = __FUNCTION__ }
+	__cleanup(__trace_cleanup) struct trace_node __concat(__trace_node, __COUNTER__) = { /* create a new unique-named node that will destruct with __trace_cleanup */ \
+		.did = __trace(t, "%*s[%s] %s: " m "\n", /* format string includes an indentation, trace name, function name, message, and newline */ \
+				f ? trace_indent_level++ : trace_indent_level, /* increment only if we're entering a new scope */ \
+				"", (t)->name, __FUNCTION__, ##__VA_ARGS__),\
+		.cleanup = f, .trace = (t), .fn = __FUNCTION__\
+	}
 
 #define TRACEFN(t,m,...) \
 	TRACE(t, true, m, ##__VA_ARGS__)
