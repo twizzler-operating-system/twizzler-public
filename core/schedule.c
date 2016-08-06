00f77e7c53b0e7430e242d4d808f2b28d5138433
Daniel Bittman <danielbittman1@gmail.com>
2016-08-06 16:09:33 -0700

    Add basic scheduler.

--- core/schedule.c
+++ core/schedule.c
@@ -16,8 +16,24 @@ static inline struct thread *choose_thread(struct processor *proc)
 	return linkedlist_remove_tail(&proc->runqueue);
 }
 
+static inline void __do_schedule(void)
+{
+	struct thread *next = choose_thread(current_thread->processor);
+	if(next != current_thread) {
+		arch_thread_switchto(current_thread, next);
+	}
+}
+
 void schedule(void)
 {
-	
+	interrupt_set_scope(false);
+	__do_schedule();
+}
+
+void preempt(void)
+{
+	interrupt_set_scope(false);
+	if(current_thread->processor->preempt_disable == 0)
+		__do_schedule();
 }
 
