c4a95a179536613077392e9b03cca3ec5da04455
Daniel Bittman <danielbittman1@gmail.com>
2016-08-19 16:46:52 -0700

    Call debug_print_backtrace from panic.

--- core/panic.c
+++ core/panic.c
@@ -2,16 +2,14 @@
 #include <debug.h>
 _Noreturn void __panic(const char *file, int linenr, int flags, const char *msg, ...)
 {
-	(void)flags;
 	va_list args;
 	va_start(args, msg);
 	printk("panic [%s:%d] - ", file, linenr);
 	vprintk(msg, args);
 	printk("\n");
 
-	/* TODO */
-	//if(flags & PANIC_UNWIND)
-	//	debug_unwind();
+	if(flags & PANIC_UNWIND)
+		debug_print_backtrace();
 	for(;;);
 }
 
