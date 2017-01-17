4af88e9a65fdc29689df4309070edcc1165f0daf
Daniel Bittman <danielbittman1@gmail.com>
2017-01-16 16:04:01 -0800

    Add toolchain build script.

---
+++ tools/toolchain.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+if [[ "$TARGET" == "" ]]; then
+	echo "Set TARGET variable before calling (e.g. x86_64-pc-elf)."
+	exit 1
+fi
+
+if [[ "$PREFIX" == "" ]]; then
+	echo "Set PREFIX variable before calling (path to install toolchain to)."
+	exit 1
+fi
+
+export PREFIX
+export TARGET
+export PATH="$PREFIX/bin:$PATH"
+
+BINUTILSVER=2.27
+GCCVER=6.3.0
+
+wget ftp://ftp.gnu.org/gnu/binutils/binutils-${BINUTILSVER}.tar.bz2
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-${GCCVER}/gcc-${GCCVER}.tar.bz2
+
+tar xf binutils-${BINTUILSVER}.tar.bz2
+tar xf gcc-${GCCVER}.tar.bz2
+
+mkdir build-binutils build-gcc
+cd build-binutils
+
+../binutils-${BINUTILSVER}/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+cd ../build-gcc
+
+../gcc-${GCCVER}/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+
+make all-gcc all-target-libgcc
+make install-gcc install-target-libgcc
+
