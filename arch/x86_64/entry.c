0ac034ccfbe72daaa3ab9b7798da32598916efdd
Daniel Bittman <danielbittman1@gmail.com>
2017-01-23 00:22:57 -0800

    [x86_64] cleanup.

--- arch/x86_64/entry.c
+++ arch/x86_64/entry.c
@@ -7,23 +7,11 @@ void x86_64_exception_entry(struct x86_64_exception_frame *frame, bool was_users
 		current_thread->arch.was_syscall = false;
 	}
 
-	//printk("Interrupt! %d %lx (%p)\n", frame->int_no, frame->rip, &current_thread->processor->arch.curr);
+	printk("Interrupt! %d %lx (%p)\n", frame->int_no, frame->rip, &current_thread->processor->arch.curr);
 	x86_64_signal_eoi();
-	asm  volatile ("xchg %%bx, %%bx" ::: "bx");
 	if(was_userspace) {
 		x86_64_resume(current_thread);
 	}
-	return;
-	/*if(frame->int_no < 32) {
-	} else {
-		//interrupt_entry(frame->int_no, frame->cs == 0x8 ? INTERRUPT_INKERNEL : 0);
-	}
-	*/
-	printk("current_thread = %p\n", current_thread);
-	for(;;);
-	//for(;;);
-	//if(frame->cs != 0x8) {
-	//}
 }
 
 void x86_64_syscall_entry(struct x86_64_syscall_frame *frame)
@@ -36,7 +24,6 @@ extern void x86_64_resume_userspace(void *);
 extern void x86_64_resume_userspace_interrupt(void *);
 void x86_64_resume(struct thread *thread)
 {
-	//printk("::- %p %p %p (%d)\n", thread->processor->arch.curr, thread, current_thread, thread->arch.was_syscall);
 	thread->processor->arch.curr = thread;
 	thread->processor->arch.tcb = (void *)((uint64_t)&thread->arch.syscall + sizeof(struct x86_64_syscall_frame));
 	thread->processor->arch.tss.esp0 = (void *)((uint64_t)&thread->arch.exception + sizeof(struct x86_64_exception_frame));
@@ -44,7 +31,6 @@ void x86_64_resume(struct thread *thread)
 	if(thread->arch.was_syscall)
 		x86_64_resume_userspace(&thread->arch.syscall);
 	else {
-	//	printk(":: %lx\n", thread->arch.exception.cs);
 		x86_64_resume_userspace_interrupt(&thread->arch.exception);
 	}
 }
