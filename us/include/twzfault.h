bf78d52587042c531430b283d14aa863ac9ab39a
Daniel Bittman <danielbittman1@gmail.com>
2018-07-05 23:09:21 -0700

    Added twzfault.h

---
+++ us/include/twzfault.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#define FAULT_OBJECT_READ  1
+#define FAULT_OBJECT_WRITE 2
+#define FAULT_OBJECT_EXEC  4
+#define FAULT_OBJECT_NOMAP 8
+#define FAULT_OBJECT_NULL  0x10
+
+struct fault_object_info {
+	uint64_t ip;
+	uint64_t addr;
+	uint64_t flags;
+	uint64_t pad;
+};
+
+
