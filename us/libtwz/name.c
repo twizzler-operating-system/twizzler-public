f2ab0803e2d50b5eaaedb385522b6599fdd74980
Daniel Bittman <danielbittman1@gmail.com>
2018-07-11 16:31:15 -0700

    Implement basic kv name resolver (part 2).

--- us/libtwz/name.c
+++ us/libtwz/name.c
@@ -7,16 +7,75 @@
 
 #include <twzkv.h>
 
+static bool name_init = false, name_data_init = false;
 static struct object name_index, name_data;
-
-#include <debug.h>
-static __attribute__((constructor)) void __init_name(void)
+static inline bool objid_parse(const char *name, objid_t *id)
 {
+	int i;
+	*id = 0;
+	int shift = 128;
+
+	for(i=0;i<33;i++) {
+		char c = *(name + i);
+		if(c == ':' && i == 16) {
+			continue;
+		}
+		if(!((c >= '0' && c <= '9')
+					|| (c >= 'a' && c <= 'f')
+					|| (c >= 'A' && c <= 'F'))) {
+			break;
+		}
+		if(c >= 'A' && c <= 'F') {
+			c += 'a' - 'A';
+		}
+
+		objid_t this = 0;
+		if(c >= 'a' && c <= 'f') {
+			this = c - 'a' + 0xa;
+		} else {
+			this = c - '0';
+		}
+
+		shift -= 4;
+		*id |= this << shift;
+	}
+	/* finished parsing? */
+	return i == 33;
+}
+
+static void __name_data_init(void)
+{
+	twzkv_create_data(&name_data);
+}
+
+static void __name_init(void)
+{
+	struct object kc;
+	if(twz_object_open(&kc, 1, FE_READ) < 0) {
+		return;
+	}
+
+	char *kcdata = twz_ptr_base(&kc);
+	char *r = strstr(kcdata, "name=");
+	if(!r) {
+		return;
+	}
+
+	char *idstr = r + 5;
+	objid_t id = 0;
+	if(!objid_parse(idstr, &id)) {
+		return;
+	}
+
+	twz_object_open(&name_index, id, FE_READ | FE_WRITE);
+
+	name_init = true;
 }
 
 static int assign_name(struct object *index, struct object *data,
 		objid_t id, const char *name)
 {
+	if(!name_init) return -1;
 	struct twzkv_item key = {
 		.data = (char *)name,
 		.length = strlen(name),
@@ -31,6 +90,7 @@ static int assign_name(struct object *index, struct object *data,
 
 static objid_t resolve_name(struct object *index, const char *name)
 {
+	if(!name_init) return 0;
 	struct twzkv_item key = {
 		.data = (char *)name,
 		.length = strlen(name),
@@ -45,6 +105,9 @@ static objid_t resolve_name(struct object *index, const char *name)
 
 objid_t twz_name_resolve(struct object *o, const char *name, uint64_t resolver)
 {
+	if(!name_init) {
+		__name_init();
+	}
 	if(resolver != NAME_RESOLVER_DEFAULT)
 		return 0;
 	objid_t id = resolve_name(&name_index, name);
@@ -59,6 +122,12 @@ objid_t twz_name_resolve(struct object *o, const char *name, uint64_t resolver)
 
 int twz_name_assign(objid_t id, const char *name, uint64_t resolver)
 {
+	if(!name_init) {
+		__name_init();
+	}
+	if(!name_data_init) {
+		__name_data_init();
+	}
 	if(resolver != NAME_RESOLVER_DEFAULT)
 		return -TE_NOTSUP;
 	return assign_name(&name_index, &name_data, id, name);
