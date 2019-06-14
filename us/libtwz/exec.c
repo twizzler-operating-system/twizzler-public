7c9656d5dc37781251f958cb27f85e1d3af0d183
Daniel Bittman <danielbittman1@gmail.com>
2019-06-14 13:59:38 -0700

    Working on exec.

--- us/libtwz/exec.c
+++ us/libtwz/exec.c
@@ -20,10 +20,30 @@ struct elf64_header {
 };
 
 extern char **environ;
-int twz_exec(objid_t id, int argc, char **argv, char **env)
+
+static int __do_exec(uint64_t idlo, uint64_t idhi, uint64_t vidlo, uint64_t vidhi, void *vector)
+{
+	struct object exe;
+	if((r = twz_object_open(&exe, id, FE_READ | FE_WRITE))) {
+		return r;
+	}
+
+	/* TODO: set fixed-point for stack */
+	struct elf64_header *hdr = twz_obj_base(&exe);
+
+	struct sys_become_args ba = {
+		.target_view = vid,
+		.target_rip = hdr->e_entry,
+		.rdi = 0,
+	};
+	return sys_become(0, &ba);
+}
+
+int twz_exec(objid_t id, char **argv, char **env)
 {
 	if(env == NULL)
 		env = environ;
+
 	objid_t vid;
 	int r;
 	if((r = twz_object_create(TWZ_OC_DFL_READ | TWZ_OC_DFL_WRITE, 0, 0, &vid))) {
@@ -31,11 +51,6 @@ int twz_exec(objid_t id, int argc, char **argv, char **env)
 	}
 	struct object view;
 	if((r = twz_object_open(&view, vid, FE_READ | FE_WRITE))) {
-		return r;
-	}
-
-	struct object exe;
-	if((r = twz_object_open(&exe, id, FE_READ | FE_WRITE))) {
 		return r;
 	}
 
@@ -56,14 +71,18 @@ int twz_exec(objid_t id, int argc, char **argv, char **env)
 		return r;
 	}
 
-	struct elf64_header *hdr = twz_obj_base(&exe);
-
-	/* TODO: set fixed point stack, copy over the args and env, and construct the rest of these args
-	 * on the new stack */
-	struct sys_become_args ba = {
-		.target_view = vid,
-		.target_rip = hdr->e_entry,
-		.rdi = 0,
-	};
-	return sys_become(0, &ba);
+	uint64_t sp;
+	uint64_t vector;
+	/* TODO: copy-in everything for the vector */
+	int ret;
+	asm volatile("movq %%rax, %%rsp\n"
+	             "call __do_exec\n"
+	             "=a"(ret)
+	             : "a"(sp),
+	             "D"((uint64_t)id),
+	             "S"((uint64_t)(id >> 64)),
+	             "d"((uint64_t)vid),
+	             "c"((uint64_t)(vid >> 64)),
+	             "r8"(vector));
+	return ret;
 }
