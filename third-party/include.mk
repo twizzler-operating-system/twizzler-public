1152e976ee8187ab6825ccf5d3841e6b03123ef6
Daniel Bittman <danielbittman1@gmail.com>
2019-07-22 13:21:26 -0700

    Fix crypt lib deps.

--- third-party/include.mk
+++ third-party/include.mk
@@ -8,25 +8,12 @@ CFLAGS_TP=$(CORE_CFLAGS) $(addprefix -I../../,$(INCLUDE_DIRS))
 INCLUDE_DIRS+=$(BUILDDIR)/third-party/libtommath/include
 INCLUDE_DIRS+=$(BUILDDIR)/third-party/libtomcrypt/include
 
-KLIBS=$(BUILDDIR)/third-party/libtommath/lib/libtommath.a
-KLIBS=$(BUILDDIR)/third-party/libtomcrypt/lib/libtomcrypt.a
+KLIBS+=$(BUILDDIR)/third-party/libtommath/lib/libtommath.a
+KLIBS+=$(BUILDDIR)/third-party/libtomcrypt/lib/libtomcrypt.a
 
 $(BUILDDIR)/third-party/libtommath/lib/libtommath.a:
 $(BUILDDIR)/third-party/libtommath/include/tommath.h:
 	make -C third-party/libtommath install DESTDIR=$$(pwd)/$(BUILDDIR)/third-party/libtommath CFLAGS="$(CFLAGS_TP) $(CFLAGS_LTM)" LIBPATH=/lib INCPATH=/include CROSS_COMPILE=$(TOOLCHAIN_PREFIX)
 
-$(BUILDDIR)/third-party/libtomcrypt/lib/libtomcrypt.a: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_custom.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_cfg.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_macros.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_cipher.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_hash.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_mac.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_prng.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_pk.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_math.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_misc.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_argchk.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_pkcs.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
-$(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
+$(BUILDDIR)/third-party/libtomcrypt/lib/libtomcrypt.a $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_custom.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_cfg.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_macros.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_cipher.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_hash.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_mac.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_prng.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_pk.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_math.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_misc.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_argchk.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt_pkcs.h $(BUILDDIR)/third-party/libtomcrypt/include/tomcrypt.h: $(BUILDDIR)/third-party/libtommath/include/tommath.h
 	make -C third-party/libtomcrypt install DESTDIR=$$(pwd)/$(BUILDDIR)/third-party/libtomcrypt CFLAGS="$(CFLAGS_TP) $(CFLAGS_LTC) $(CFLAGS_LTC_DEFS)" LIBPATH=/lib INCPATH=/include CROSS_COMPILE=$(TOOLCHAIN_PREFIX)
