eff0c7ce6b22ea8f07a67fd88a8f230d63dd2bfd
Daniel Bittman <danielbittman1@gmail.com>
2019-11-27 14:01:09 -0800

    Add kso.c

---
+++ us/libtwz/kso.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <twz/obj.h>
+
+int kso_set_name(twzobj *obj, const char *name, ...)
+{
+	va_list va;
+	va_start(va, name);
+	struct kso_hdr *hdr = obj ? twz_object_base(obj) : twz_thread_repr_base();
+	int r = vsnprintf(hdr->name, KSO_NAME_MAXLEN, name, va);
+	va_end(va);
+	return r;
+}
--- us/libtwz/object.c
+++ us/libtwz/object.c
@@ -8,16 +8,6 @@
 #include <twz/sys.h>
 #include <twz/thread.h>
 #include <twz/view.h>
-int kso_set_name(twzobj *obj, const char *name, ...)
-{
-	va_list va;
-	va_start(va, name);
-	struct kso_hdr *hdr = obj ? twz_object_base(obj) : twz_thread_repr_base();
-	int r = vsnprintf(hdr->name, KSO_NAME_MAXLEN, name, va);
-	va_end(va);
-	return r;
-}
-
 int twz_object_create(int flags, objid_t kuid, objid_t src, objid_t *id)
 {
 	if(flags & TWZ_OC_ZERONONCE) {
