01770545fdd163b979ce2d8ca0328383177b31a0
Daniel Bittman <danielbittman1@gmail.com>
2020-01-28 10:46:11 -0800

    Add pmap.h

---
+++ include/pmap.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <page.h>
+
+#define PMAP_WB PAGE_CACHE_WB
+#define PMAP_UC PAGE_CACHE_UC
+#define PMAP_WT PAGE_CACHE_WT
+#define PMAP_WC PAGE_CACHE_WC
+
+void *pmap_allocate(uintptr_t phys, size_t len, int flags);
