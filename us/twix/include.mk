0055784caa36b7791e15958568ad7224f824bcb7
Daniel Bittman <danielbittman1@gmail.com>
2020-04-13 23:40:51 -0700

    Adding ubsan to twix.

--- us/twix/include.mk
+++ us/twix/include.mk
@@ -1,9 +1,16 @@
 TWIX_SRC=$(addprefix us/twix/,syscall.c rw.c fd.c linux.c file.c thread.c access.c process.c time.c)
 TWIX_OBJ=$(addprefix $(BUILDDIR)/,$(TWIX_SRC:.c=.o))
 
+TWIXCFLAGS=-fsanitize=undefined -g
+
 $(BUILDDIR)/us/twix/libtwix.a: $(TWIX_OBJ)
 	@echo "[AR]      $@"
-	@ar rcs $(BUILDDIR)/us/twix/libtwix.a $(TWIX_OBJ)
+	#@ar rcs $(BUILDDIR)/us/twix/libtwix.a $(TWIX_OBJ)
+	@mkdir -p $(dir $@)/tmp
+	@echo "[ARx]     libubsan.a"
+	@cd $(dir $@)/tmp && ar x $$($(TWZCC) -print-file-name=libubsan.a)
+	@echo "[AR]      $@"
+	@ar rcs $(BUILDDIR)/us/twix/libtwix.a $(TWIX_OBJ) $(BUILDDIR)/us/twix/tmp/*.o
 
 $(BUILDDIR)/us/twix/libtwix.so: $(TWIX_OBJ)
 	@echo "[LD]      $@"
@@ -12,7 +19,7 @@ $(BUILDDIR)/us/twix/libtwix.so: $(TWIX_OBJ)
 $(BUILDDIR)/us/twix/%.o: us/twix/%.c $(MUSL_HDRS)
 	@echo "[CC]      $@"
 	@mkdir -p $(BUILDDIR)/us/twix
-	@$(TWZCC) $(TWZCFLAGS) -c -o $@ $< -MD -fPIC
+	$(TWZCC) $(TWZCFLAGS) $(TWIXCFLAGS) -c -o $@ $< -MD -fPIC
 
 -include $(TWIX_OBJ:.o=.d)
 
