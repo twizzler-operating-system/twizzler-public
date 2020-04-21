40e882bc749b1d46ad58038701e4e2e1bd19ecd6
Daniel Bittman <danielbittman1@gmail.com>
2020-04-21 14:00:07 -0700

    Update config.mk.

--- tools/prep.sh
+++ tools/prep.sh
@@ -31,4 +31,4 @@ $PROJECT. The toolchains will be installed to \`$(pwd)/.tc'. You can change this
 projects/$PROJECT/config.mk. Press enter to continue."
 read
 
-sed -i "s|TOOLCHAIN_PATH=|TOOLCHAIN_PATH=$(pwd)/.tc|g" projects/$PROJECT/config.mk
+sed -i "s|TOOLCHAIN_PATH=.*|TOOLCHAIN_PATH=$(pwd)/.tc|g" projects/$PROJECT/config.mk
